4d57ae39f56e9562221482a7c11ec30dfe3eccca
Cybiaé <Cybiae.Cornet@etu-paris1.fr>
2024-11-04 15:20:39 +0100

    récupération des information de la base de donnée et changement css

--- Liste_films_php.php
+++ Liste_films_php.php
@@ -1,16 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$dbname = "movie";
+
+// Créer la connexion
+$connexion = new mysqli($servername, $username,"", $dbname);
+?>
 <html> 
-    <head> <title> La liste des films</title> </head>
+    <head> <title> La liste des films</title> 
+    <link rel="stylesheet" href="Liste_film_css.css" type="text/css" />
+    </head>
     <body>
     <article>
-    <h1> Voici nos films </h1> <?php 
-for($i =0; $i<=3; $i++){
-    
-
+    <h1> Voici nos films </h1> 
+<?php 
+$info_film = "SELECT * FROM info_movie";
+$result = $connexion->query($info_film);
+while($row = $result->fetch_assoc()){
+    ?><article class ="information_films">
+    <?php
+    echo "<h3>Nom de film</h3> " . $row["titrefilm"] . " " . $row["year"] . "<br>" . $row["synopsis"] . "<br> " . $row["directeur"] . "<br> " . $row["genre"] . "<br>";
+    ?></article>
+<?php
+}
 ?>
-<article>
- <h3>Nom de film </h3>
-    <p> Résumé du films </p> 
-</article>
-<?php }?>
+
+<?php ?>
 </article></body>
 </html>
