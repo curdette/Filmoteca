2d77a2ebb228fe7fb485b8a7eaf789eaac3b4536
Cybiaé <Cybiae.Cornet@etu-paris1.fr>
2024-11-20 09:40:49 +0100

    petit ajout film controlleur

--- src/controller/FilmController.php
+++ src/controller/FilmController.php
@@ -5,5 +5,20 @@ namespace App\Controller;
 class FilmController
 {
     // Code de ta classe FilmController ici...
+    public function create()
+    {
+    }
+
+    public function read()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
 }
 ?>
