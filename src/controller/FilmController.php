e5724f3fed847223fc81c4b4f5d940b33316b24e
Cybiaé <Cybiae.Cornet@etu-paris1.fr>
2024-11-20 16:21:54 +0100

    Update du repository et du fichier json

--- src/controller/FilmController.php
+++ src/controller/FilmController.php
@@ -4,37 +4,45 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\FilmEntity;
+use App\Entity\Film;
 use App\Repository\FilmRepository;
 
 class FilmController
 {
-    public function list()
+    public function list(array $queryParams)
     {
         $filmRepository = new FilmRepository();
         $films = $filmRepository->findAll();
 
-        header('Content-Type: application/json');
-        echo json_encode($films);
-       /* foreach( $films as $film ){
-            $filmEntity = new FilmEntity();
-            $filmEntity->setTitle($film['titre']);
-            $filmEntity->setSynopsis($film[synopsis]);
-            $filmEntity->setDirector($[director]));
-        }*/
+        /* $filmEntities = [];
+        foreach ($films as $film) {
+            $filmEntity = new Film();
+            $filmEntity->setId($film['id']);
+            $filmEntity->setTitle($film['title']);
+            $filmEntity->setYear($film['year']);
+            $filmEntity->setType($film['type']);
+            $filmEntity->setSynopsis($film['synopsis']);
+            $filmEntity->setDirector($film['director']);
+            $filmEntity->setCreatedAt(new \DateTime($film['created_at']));
+            $filmEntity->setUpdatedAt(new \DateTime($film['updated_at']));
+
+            $filmEntities[] = $filmEntity;
+        } */
+
+        dd($films);
+
+        // header('Content-Type: application/json');
+        // echo json_encode($films);
     }
 
     public function create()
     {
         echo "Création d'un film";
-
     }
 
     public function read()
     {
         echo "Lecture d'un film";
-        //regarde l'id après le read dans l'url, puis on va regarder le tableau d'objet pour trouver celui qui à un id=7
-        //On récupère les information grace au tableau d'objet et on l'affiche
     }
 
     public function update()
